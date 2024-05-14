54400f210d548140eb9ad3976918912482dc819e
Salma <salmanasser8456@gmail.com>
2024-05-15 00:41:48 +0300

    Template

--- C++/generic/Template/template_function.cpp
+++ C++/generic/Template/template_function.cpp
@@ -5,7 +5,8 @@
 template<typename type>
 
 type max(type n1, type n2){
-    return n1 > n2? n1: n2;
+    type temp = n1 > n2;
+    return temp;
 }
 
 template<class T>
@@ -16,14 +17,52 @@ void swap(T& first, T& second){
 }
 
 
+class Person{
+    public:
+    Person(int p_Id, int p_Age): Id(p_Id), Age(p_Age){
+
+    }
+
+    Person(Person &per){
+        this->Age = per.Age;
+        this->Id = per.Id;
+    }
+
+    Person operator > (Person & p2){
+        if(p2.Age > this->Age) return p2;
+        return *this;
+    } 
+
+    void operator = (Person &p2){
+        this->Age = p2.Age;
+        this->Id = p2.Id;
+    }
+
+    int getAge(){
+        return this->Age;
+    }
+
+    int getId(){
+        return this->Id;
+    }
+
+    private:
+    int Age;
+    int Id;
+};
+
+
 int main(){
-    int first = 7;
-    int second = 5;
+    Person p1(1, 25);
+    Person p2(2, 26);
 
-    double n1 = 0.5;
-    double n2 = 7.15;
+    swap<Person>(p1, p2);
 
-    std::cout << "max is " << max(first, second) << std::endl;
-    std::cout << "max is " << max(n1, n2) << std::endl;
+    std::cout << "P1 age = " << p1.getAge() << ", id = " << p1.getId() << std::endl;
+    std::cout << "P2 age = " << p2.getAge() << ", id = " << p2.getId() << std::endl;
+
+    Person p3 = max<Person>(p1, p2);
+
+    std::cout << "Id of Max = " << p1.getId() << std::endl; 
 
 }
